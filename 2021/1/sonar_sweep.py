8d0095159cd5193f911d15fda17dc6b7a92db564
Alexandre Pilleyre <alexandre.pilleyre@grenoble-inp.org>
2021-12-01 22:54:38 +0100

    Formatting and renaming day 1 files

--- 2021/1/sonar_sweep.py
+++ 2021/1/sonar_sweep.py
@@ -1,5 +1,4 @@
-
-INPUT_FILE = './sonar_sweep_part_1_input.txt'
+INPUT_FILE = './sonar_sweep_input.txt'
 
 def extract_data(input_file):
     with open(INPUT_FILE, 'r') as f:
@@ -17,15 +16,17 @@ def get_increase_count(values):
 def three_value_sliding_window_sum(values):
     agglomaerated_values = []
     for i in range(len(values) - 2):
-        agglomaerated_values.append(int(values[i]) + int(values[i + 1]) + int(values[i + 2]))
+        agglomaerated_values.append(
+            int(values[i]) + int(values[i + 1]) + int(values[i + 2]))
     return agglomaerated_values
 
 def part1(depth_values):
-    print(get_increase_count(depth_values))
+    print(f"First part: {get_increase_count(depth_values)}")
+
 
 def part2(depth_values):
     agglomaerated_values = three_value_sliding_window_sum(depth_values)
-    print(get_increase_count(agglomaerated_values))
+    print(f"Second part: {get_increase_count(agglomaerated_values)}")
 
 def main():
     depth_values = extract_data(INPUT_FILE)

a9328c016d0afe092a2678367aff4d6a681d4cf4
Alexandre Pilleyre <alexandre.pilleyre@grenoble-inp.org>
2021-12-13 14:57:26 +0100

    Added skeletoon for day 10

---
+++ 2021/10/syntax_scoring.py
@@ -0,0 +1,21 @@
+INPUT_FILE = f"{__file__}.replace('.py', '_input.txt')}"
+INPUT_FILE = f"{__file__}.replace('.py', '_input_small.txt')}"
+
+def get_input(filename):
+    pass
+
+def part1(values):
+    pass
+
+def part2(values):
+    pass
+
+def main()
+    values = get_input(INPUT_FILE)
+    # values = get_input(INPUT_FILE_SMALL)
+    part1(values)
+    part2(values)
+
+if __name__ == "__main__":
+    main()
+

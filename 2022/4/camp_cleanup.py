51f1a041b972fb26497629bbbace2a7e28266fe7
Alexandre Pilleyre <pilleyre.alexandre@gmail.com>
2022-12-05 11:55:51 +0100

    added sources for 12/5/2022

---
+++ 2022/4/camp_cleanup.py
@@ -0,0 +1,53 @@
+INPUT_FILE = f"{__file__.replace('.py', '_input.txt')}"
+# INPUT_FILE_SMALL = f"{__file__.replace('.py', '_input_small.txt')}"
+
+class MyRange:
+    def __init__(self, start, end):
+        self.start = int(start)
+        self.end = int(end)
+
+    def isFullyContaining(self, other):
+        return self.start <= other.start and self.end >= other.end
+
+    def isOverlapping(self, other):
+        return max(self.start, other.start) <= min(self.end, other.end)
+
+    def __repr__(self) -> str:
+        return f"{self.start}-{self.end}"
+    def __str__(self):
+        return f"{self.start}-{self.end}"
+
+def get_input(file):
+    with open(file) as f:
+        return [[ MyRange(*j.split('-')) for j in i.split(',')] for i in f.read().splitlines()]
+
+
+
+def get_fully_containing_count(values):
+    count = 0
+    for r1, r2 in values:
+        if r1.isFullyContaining(r2) or r2.isFullyContaining(r1):
+            count += 1
+    return count
+
+def get_overlapping_count(values):
+    count = 0
+    for r1, r2 in values:
+        if r1.isOverlapping(r2):
+            count += 1
+    return count
+
+def part1(values):
+    print(f"First part: {get_fully_containing_count(values)}")
+
+def part2(values):
+    print(f"Second part: {get_overlapping_count(values)}")
+
+def main():
+    values = get_input(INPUT_FILE)
+    # values = get_input(INPUT_FILE_SMALL)
+    part1(values) # First part: 496
+    part2(values) # Second part:
+
+if __name__ == "__main__":
+    main()

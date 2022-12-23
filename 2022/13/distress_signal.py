273c37881dd99a9b95c8f73363f0bda1a5ff5ff4
Alexandre Pilleyre <pilleyre.alexandre@gmail.com>
2022-12-23 19:18:03 +0100

    some facto for 2022/12/13

--- 2022/13/distress_signal.py
+++ 2022/13/distress_signal.py
@@ -5,8 +5,7 @@ from functools import cmp_to_key
 
 def get_input(file):
     with open(file) as f:
-        pair_of_packet_raw = [i for i in f.read().splitlines()]
-        return list(map(eval, filter(lambda x: x != '', pair_of_packet_raw)))
+        return [[*map(eval, i.split())] for i in f.read().split('\n\n')]
 
 def pair_list_compare(pair: list[list]):
     left_iter = iter(pair[0])
@@ -26,26 +25,28 @@ def pair_list_compare(pair: list[list]):
 def pair_compare(pair: list):
     left = pair[0]
     right = pair[1]
-    if type(left) is int and type(right) is int:
-        return left - right
-    if type(left) is int:
-        left = [left]
-    if type(right) is int:
-        right = [right]
-    return pair_list_compare([left, right])
+    match left, right:
+        case int(), int():
+            return left - right
+        case int(), list():
+            return pair_compare([[left], right])
+        case list(), int():
+            return pair_compare([left, [right]])
+        case list(), list():
+            return pair_list_compare([left, right])
 
 def right_order_pairs_indices_sum(values):
     count = 0
-    for i in range(0, len(values), 2):
-        compare = pair_compare([values[i], values[i+1]])
+    for i, pair in enumerate(values):
+        compare = pair_compare(pair)
         if compare < 0:
-            count += i//2 + 1
+            count += i + 1
     return count
 
 def divider_packets(values):
     first_val, second_val = [[2]], [[6]]
-    values = values + [first_val, second_val]
-    sorted_values = sorted(values, key=cmp_to_key(lambda a, b: pair_compare([a, b])))
+    vals = [first_val, second_val]
+    sorted_values = sorted(sum(values, vals), key=cmp_to_key(lambda a, b: pair_compare([a, b])))
     return (sorted_values.index(first_val) + 1) * (sorted_values.index(second_val) + 1)
 
 def part1(values):
@@ -62,3 +63,14 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Another solution for comparison:
+# def cmp(l, r):
+#     match l, r:
+#         case int(), int():  return (l>r) - (l<r)
+#         case int(), list(): return cmp([l], r)
+#         case list(), int(): return cmp(l, [r])
+#         case list(), list():
+#             for z in map(cmp, l, r):
+#                 if z: return z
+#             return cmp(len(l), len(r))

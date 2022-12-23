5145ccfcf106fc9543dd462c63e2604d5611cc8c
Alexandre Pilleyre <pilleyre.alexandre@gmail.com>
2022-12-23 19:02:18 +0100

    Finished 2022/12/13

--- 2022/13/distress_signal.py
+++ 2022/13/distress_signal.py
@@ -1,22 +1,26 @@
 INPUT_FILE = f"{__file__.replace('.py', '_input.txt')}"
-# INPUT_FILE_SMALL = f"{__file__.replace('.py', '_input_small.txt')}"
+INPUT_FILE_SMALL = f"{__file__.replace('.py', '_input_small.txt')}"
+
+from functools import cmp_to_key
 
 def get_input(file):
     with open(file) as f:
-        pair_of_packet_raw = [i for i in f.read().split('\n\n')]
-        return [[eval(j) for j in i.splitlines()] for i in pair_of_packet_raw]
+        pair_of_packet_raw = [i for i in f.read().splitlines()]
+        return list(map(eval, filter(lambda x: x != '', pair_of_packet_raw)))
 
 def pair_list_compare(pair: list[list]):
     left_iter = iter(pair[0])
     right_iter = iter(pair[1])
     curr_l = next(left_iter, None)
     curr_r = next(right_iter, None)
-    while curr_l and curr_r:
+    while curr_l is not None and curr_r is not None:
         diff = pair_compare([curr_l, curr_r])
         if diff != 0:
             return -1 if diff < 0 else 1
         curr_l = next(left_iter, None)
         curr_r = next(right_iter, None)
+    if curr_l is None and curr_r is None:
+        return 0
     return -1 if curr_l is None else 1
 
 def pair_compare(pair: list):
@@ -32,22 +36,29 @@ def pair_compare(pair: list):
 
 def right_order_pairs_indices_sum(values):
     count = 0
-    for idx, pair in enumerate(values):
-        if pair_compare(pair) < 0:
-            count += idx + 1
+    for i in range(0, len(values), 2):
+        compare = pair_compare([values[i], values[i+1]])
+        if compare < 0:
+            count += i//2 + 1
     return count
+
+def divider_packets(values):
+    first_val, second_val = [[2]], [[6]]
+    values = values + [first_val, second_val]
+    sorted_values = sorted(values, key=cmp_to_key(lambda a, b: pair_compare([a, b])))
+    return (sorted_values.index(first_val) + 1) * (sorted_values.index(second_val) + 1)
 
 def part1(values):
     print(f"First part: {right_order_pairs_indices_sum(values)}")
 
 def part2(values):
-    print(f"Second part:")
+    print(f"Second part: {divider_packets(values)}")
 
 def main():
     values = get_input(INPUT_FILE)
     # values = get_input(INPUT_FILE_SMALL)
-    part1(values)  # First part:
-    part2(values)  # Second part:
+    part1(values)  # First part: 5717
+    part2(values)  # Second part: 25935
 
 if __name__ == "__main__":
     main()
